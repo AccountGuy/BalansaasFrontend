84f036a0e93a33589c9fe7b1f0dac4f608d99a82
alvaro14s <escalona.alvaro.13@gmail.com>
2024-04-22 01:53:46 -0400

    9-components: Update classes for layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,7 +6,7 @@ export const Route = createRootRoute({
   component: () => (
     <div className="min-w-full min-h-svh relative flex">
       <Sidebar />
-      <main className="flex-1">
+      <main className="flex-1 px-6 py-4">
         <Outlet />
       </main>
       <TanStackRouterDevtools />
