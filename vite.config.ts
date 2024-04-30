eb31d7ab20de70b3322a79e09c77f59028a8311d
alvaro14s <escalona.alvaro.13@gmail.com>
2024-04-29 21:17:19 -0400

    16-root: Add env port

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 import react from '@vitejs/plugin-react'
+import viteReact from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.PORT) || 3001
+  },
   plugins: [
     react(),
     TanStackRouterVite(),
