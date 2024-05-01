ffa96344aa8bd635dc66cc7b6a33e16bcc418f77
alvaro14s <escalona.alvaro.13@gmail.com>
2024-04-30 22:43:57 -0400

    26-routes: Add management of routes for authentication

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,19 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import Sidebar from "../components/Sidebar";
+import Login from "../components/Login";
+import { useLoginStore } from "../stores/authStore";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: RootHandler,
+});
+
+function RootHandler() {
+  const { token: isAuthenticated } = useLoginStore();
+  console.log(isAuthenticated);
+
+  if (!isAuthenticated) return <Login />;
+  return (
     <div className="min-w-full min-h-svh relative flex">
       <Sidebar />
       <main className="flex-1 px-6 py-4">
@@ -11,5 +21,5 @@ export const Route = createRootRoute({
       </main>
       <TanStackRouterDevtools position="bottom-right" />
     </div>
-  ),
-});
+  );
+}
