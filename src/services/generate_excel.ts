091b5c40a48c7aac389d395321d7fc21604f46d3
alvaro14s <escalona.alvaro.13@gmail.com>
2024-05-07 14:01:39 -0400

    35-services: Add fill of DATOS worksheet

--- src/services/generate_excel.ts
+++ src/services/generate_excel.ts
@@ -1,12 +1,6 @@
 import ExcelJS from 'exceljs'
-const jsonString = '[{"code":548,"value":0},{"code":520,"value":1835203},{"code":762,"value":0},{"code":766,"value":1348714},{"code":525,"value":0},{"code":528,"value":258386},{"code":532,"value":185221},{"code":535,"value":0},{"code":553,"value":0},{"code":562,"value":1929772},{"code":514,"value":0},{"code":521,"value":0},{"code":62,"value":0},{"code":49,"value":1030107},{"code":155,"value":0},{"code":54,"value":0},{"code":56,"value":0},{"code":151,"value":513374},{"code":153,"value":0},{"code":48,"value":238264}]';
-
-interface DataSheetRowDataProps {
-  code: number
-  value: number
-}
-
-const exampleData = JSON.parse(jsonString) as DataSheetRowDataProps[];
+import type { Worksheet } from 'exceljs'
+import { jsonToTableData, exampleData, obtainFiscalCredit, obtainFiscalDebit, obtainExportation, obtainExemptExpenses, obtainTransitoryFees, obtainWithholdingFees, obtainUniqueFee } from './sii_formulas';
 
 export async function excelGeneration() {
   try {
@@ -21,17 +15,31 @@ export async function excelGeneration() {
   }
 }
 
-export async function generateExcel(blob: any) {
+async function generateExcel(blob: any) {
   const workbook = new ExcelJS.Workbook();
   await workbook.xlsx.load(blob);
 
-  const dataSheetName = 'BRUTO'
-  const worksheetData = workbook.addWorksheet(dataSheetName);
+  const supportSheetName = 'BRUTO'
+  const worksheetSupport = workbook.addWorksheet(supportSheetName)
 
-  // Add content to a specific cell
-  const rowData = exampleData.map((row) => [row.code, row.value])
-  worksheetData.addRow(['code', 'value'])
-  worksheetData.addRows(rowData)
+  // Add the support information
+  worksheetSupport.addRow(['año', 'mes', 'code', 'value'])
+  worksheetSupport.addRows(jsonToTableData(exampleData))
+
+  // Fill the cells with the aggregate data
+  const dataSheetName = 'DATOS'
+  const worksheetData = workbook.getWorksheet(dataSheetName);
+
+  if (worksheetData !== undefined) {
+    saveFiscalDebt(worksheetData)
+    savePPM(worksheetData)
+    saveTransitoryFees(worksheetData)
+    saveWithholdingFees(worksheetData)
+    // Next Table
+    saveFiscalCredit(worksheetData)
+    saveExemptExpenses(worksheetData)
+    saveUniqueFee(worksheetData)
+  }
 
   // Save the workbook
   const buffer = await workbook.xlsx.writeBuffer();
@@ -41,4 +49,72 @@ export async function generateExcel(blob: any) {
   a.href = url;
   a.download = 'balansaas_excel.xlsx';
   a.click();
+}
+
+function saveFiscalDebt(worksheet: Worksheet) {
+  const offsetRow = 14
+  const offsetColumn = 5
+  exampleData.map(monthData => {
+    const cellData = obtainFiscalDebit(monthData.information)
+    worksheet.getCell(offsetRow + monthData.month, offsetColumn).value = cellData
+  })
+}
+
+function savePPM(worksheet: Worksheet) {
+  // Pagos provisionales mensuales (62)
+  const offsetRow = 14
+  const offsetColumn = 6
+  exampleData.map(monthData => {
+    const cellData = obtainExemptExpenses(monthData.information)
+    worksheet.getCell(offsetRow + monthData.month, offsetColumn).value = cellData
+  })
+}
+
+function saveWithholdingFees(worksheet: Worksheet) {
+  // Responds 'Retencion de honorarios'
+  const offsetRow = 14
+  const offsetColumn = 7
+  exampleData.map(monthData => {
+    const cellData = obtainWithholdingFees(monthData.information)
+    worksheet.getCell(offsetRow + monthData.month, offsetColumn).value = cellData
+  })
+}
+
+function saveTransitoryFees(worksheet: Worksheet) {
+  // Respond for Impuestos transitorios [153 + 49]
+  const offsetRow = 14
+  const offsetColumn = 8
+  exampleData.map(monthData => {
+    const cellData = obtainTransitoryFees(monthData.information)
+    worksheet.getCell(offsetRow + monthData.month, offsetColumn).value = cellData
+  })
+}
+
+function saveFiscalCredit(worksheet: Worksheet) {
+  const offsetRow = 35
+  const offsetColumn = 5
+  exampleData.map(monthData => {
+    const cellData = obtainFiscalCredit(monthData.information) + obtainExportation(monthData.information)
+    worksheet.getCell(offsetRow + monthData.month, offsetColumn).value = cellData
+  })
+}
+
+function saveExemptExpenses(worksheet: Worksheet) {
+  // Respond for 'Gastos exentos'
+  const offsetRow = 35
+  const offsetColumn = 6
+  exampleData.map(monthData => {
+    const cellData = obtainExemptExpenses(monthData.information)
+    worksheet.getCell(offsetRow + monthData.month, offsetColumn).value = cellData
+  })
+}
+
+function saveUniqueFee(worksheet: Worksheet) {
+  // Respond for 'Impuesto único'
+  const offsetRow = 35
+  const offsetColumn = 9
+  exampleData.map(monthData => {
+    const cellData = obtainUniqueFee(monthData.information)
+    worksheet.getCell(offsetRow + monthData.month, offsetColumn).value = cellData
+  })
 }
