7c708298d1f473b8b67e1c3f2eab327452e1f60d
alvaro14s <escalona.alvaro.13@gmail.com>
2024-05-23 10:24:58 -0400

    63-layouts: Use custom hook to feedback the background process

--- src/layouts/F29Layout.tsx
+++ src/layouts/F29Layout.tsx
@@ -1,22 +1,40 @@
-import Form29 from "@/custom_components/Form29";
-import { getSelectAccount } from "@/handlers/accountsHandler";
-import { useQuery } from "@tanstack/react-query";
+import Form29 from '@/custom_components/Form29'
+import { getSelectAccount } from '@/handlers/accountsHandler'
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import useActionCableHook from '@/hooks/useActioncableHook'
 
 const F29Layout = () => {
+  const [notifications, setNotifications] = useState<any>([])
   const { data, isLoading, isError } = useQuery({
     queryFn: getSelectAccount,
-    queryKey: ["account-select"],
-  });
+    queryKey: ['account-select'],
+  })
+
+  const handleReceivedMessage = (message: any) => {
+    console.log('Message', message)
+    setNotifications([...notifications, message])
+  }
+
+  useActionCableHook('NotificationChannel', handleReceivedMessage)
+  console.log(notifications)
+
   return (
     <main className="flex flex-row flex-wrap gap-5">
-      <article className="flex flex-col flex-1">
+      <article className="flex flex-1 flex-col">
         <h1 className="mb-4">Formulario 29</h1>
-        {isError ?? "Error during loading"}
-        {isLoading ? "Loading" : <Form29 accounts={data!} />}
+        {isError ?? 'Error during loading'}
+        {isLoading ? 'Loading' : <Form29 accounts={data!} />}
       </article>
-      <article className="flex-1" data-testid="previewData"></article>
+      <article className="flex-1" data-testid="previewData">
+        <section>
+          {notifications.map((notification: any) => (
+            <div>{notification.message}</div>
+          ))}
+        </section>
+      </article>
     </main>
-  );
-};
+  )
+}
 
-export default F29Layout;
+export default F29Layout
