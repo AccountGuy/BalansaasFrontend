12c55600493ffe9898a189a6f4258fafa0f7bf9e
alvaro14s <escalona.alvaro.13@gmail.com>
2024-05-23 10:22:36 -0400

    54-custom_components: Format prettier

--- src/custom_components/Form29.tsx
+++ src/custom_components/Form29.tsx
@@ -1,50 +1,48 @@
-import { FormEvent, useState } from "react";
-import { excelGeneration } from "@/services/generate_excel";
+import { FormEvent, useState } from 'react'
+import { excelGeneration } from '@/services/generate_excel'
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
-import type { AccountSelect } from "@/schemas";
-import { getDateRanges } from "@/lib/date_utils";
-import { postForSiiFormRecords } from "@/handlers/siiFormRecordHandler";
-import { useMutation } from "@tanstack/react-query";
+} from '@/components/ui/select'
+import type { AccountSelect } from '@/schemas'
+import { getDateRanges } from '@/lib/date_utils'
+import { postForSiiFormRecords } from '@/handlers/siiFormRecordHandler'
+import { useMutation } from '@tanstack/react-query'
 
 interface Form29Props {
-  accounts: AccountSelect[];
+  accounts: AccountSelect[]
 }
 
-const years = getDateRanges().reverse();
+const years = getDateRanges().reverse()
 
 const Form29 = ({ accounts }: Form29Props) => {
-  const [selectedAccount, setSelectedAccount] = useState<number>();
-  const [selectedYear, setSelectedYear] = useState<number>();
+  const [selectedAccount, setSelectedAccount] = useState<number>()
+  const [selectedYear, setSelectedYear] = useState<number>()
   // const { register, handleSubmit } = useForm<SiiFormRecordProps>();
   const { mutateAsync, isPending, isError } = useMutation({
     mutationFn: postForSiiFormRecords,
-  });
+  })
 
   const handleSubmitAction = async (e: FormEvent) => {
-    e.preventDefault();
+    e.preventDefault()
     const f29Data = await mutateAsync({
       account_id: selectedAccount as number,
       year: selectedYear as number,
-    });
+    })
     if (!isError && !isPending) {
-      await excelGeneration(f29Data);
+      await excelGeneration(f29Data)
     }
-  };
+  }
 
   return (
     <article className="min-w-80 max-w-96">
       <form onSubmit={handleSubmitAction}>
         <section className="form-field">
           <label className="label-field">Selecciona la cuenta</label>
-          <Select
-            onValueChange={(account) => setSelectedAccount(Number(account))}
-          >
+          <Select onValueChange={(account) => setSelectedAccount(Number(account))}>
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Selecciona la cuenta" />
             </SelectTrigger>
@@ -86,7 +84,7 @@ const Form29 = ({ accounts }: Form29Props) => {
         </section>
       </form>
     </article>
-  );
-};
+  )
+}
 
-export default Form29;
+export default Form29
