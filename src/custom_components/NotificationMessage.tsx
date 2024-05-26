b71aa1866eb857ac72ec7cd29aa54c422a51e25d
alvaro14s <escalona.alvaro.13@gmail.com>
2024-05-26 17:18:35 -0400

    76-custom-components: Add more options for NotificationMessage

--- src/custom_components/NotificationMessage.tsx
+++ src/custom_components/NotificationMessage.tsx
@@ -1,9 +1,6 @@
+import NormalInfo from '@/components/icons/NormalInfo'
+import { WebSocketNotification } from '@/schemas'
 import { Check, X } from 'lucide-react'
-
-interface WSNotificationProps {
-  message: string
-  kind: 'success' | 'error'
-}
 
 const style = {
   success: {
@@ -11,23 +8,30 @@ const style = {
     color: 'text-emerald-600',
   },
   error: { border: 'border-red-600', color: 'text-red-600' },
+  info: { border: 'border-sky-600', color: 'text-sky-600' },
+  none: { border: 'border-cyan-600', color: 'text-cyan-600' },
 } as const
 
-const NotificationMessage = ({ message, kind }: WSNotificationProps) => {
+const NotificationMessage = ({ message, kind }: WebSocketNotification) => {
+  const iconSelected =
+    {
+      success: <Check />,
+      error: <X />,
+      info: <NormalInfo />,
+      none: null,
+    }[kind] ?? null
+
   return (
-    <section className="flex max-w-96 border border-gray-100 shadow-md">
+    <section className="flex w-full border border-gray-100 shadow-md">
       <div
-        className={`flex w-full items-center gap-2 border-l-4 ${style[kind].border} py-4 pl-4 pr-8`}
+        className={`flex w-full items-center justify-between gap-2 border-l-4 ${style[kind].border} py-4 pl-4 pr-8`}
       >
-        <span className="flex-1 pl-0.5 font-medium">{message}</span>
-        <span className={`${style[kind].color} ${style[kind].border} rounded-full border-2 p-1`}>
-          {
-            {
-              success: <Check />,
-              error: <X />,
-            }[kind]
-          }
-        </span>
+        <div className="flex-1 pl-0.5 text-left font-medium">{message}</div>
+        {iconSelected ? (
+          <span className={`${style[kind].color} ${style[kind].border} rounded-full border-2 p-1`}>
+            {iconSelected}
+          </span>
+        ) : null}
       </div>
     </section>
   )
