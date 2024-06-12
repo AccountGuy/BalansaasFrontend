a030ad05a4d9c9d55b420fb889e66eb31aacf7bb
alvaro14s <escalona.alvaro.13@gmail.com>
2024-06-12 19:46:39 -0400

    103-routes: Remove TanStack router devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { AuthProps } from '@/stores/authStore'
 import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
 export const Route = createRootRouteWithContext<AuthProps>()({
   component: RootHandler,
@@ -10,7 +9,6 @@ function RootHandler() {
   return (
     <div className="relative flex h-full min-w-full">
       <Outlet />
-      <TanStackRouterDevtools position="bottom-right" />
     </div>
   )
 }
